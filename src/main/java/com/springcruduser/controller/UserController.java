bb7b953f38c8f97488a2119a571b2d8fd298c7e8
ehy0419 <2hy0419@naver.com>
2025-08-01 13:50:33 +0900

    service 까지 따라치기 완성

--- src/main/java/com/springcruduser/controller/UserController.java
+++ src/main/java/com/springcruduser/controller/UserController.java
@@ -4,9 +4,9 @@ import com.springcruduser.dto.UserRequest;
 import com.springcruduser.dto.UserResponse;
 import com.springcruduser.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,5 +17,33 @@ public class UserController {
     @PostMapping("/users")
     public UserResponse createUser(
             @RequestBody UserRequest userRequest
-    )
+    ) {
+        return userService.save(userRequest);
+    }
+
+    @GetMapping("/users")
+    public List<UserResponse> getUsers() {
+        return userService.findUsers();
+    }
+
+    @GetMapping("user/{userId}")
+    public UserResponse getUser(
+            @PathVariable Long userId) {
+        return userService.finduser(userId);
+    }
+
+    @PutMapping("/user/{userId}")
+    public UserResponse updateUser(
+            @PathVariable Long userId,
+            @RequestBody UserRequest userRequest
+    ) {
+        return userService.update(userId, userRequest);
+    }
+
+    @DeleteMapping("/user/{uersId}")
+    public void deleteUser(
+            @PathVariable Long userId
+    ) {
+        userService.deleteUser(userId);
+    }
 }
--- src/main/java/com/springcruduser/service/UserService.java
+++ src/main/java/com/springcruduser/service/UserService.java
@@ -1,9 +1,66 @@
 package com.springcruduser.service;
 
+import com.springcruduser.dto.UserRequest;
+import com.springcruduser.dto.UserResponse;
+import com.springcruduser.entity.User;
+import com.springcruduser.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    @Transactional
+    public UserResponse save(UserRequest userRequest) {
+        User savedUser = userRepository.save(new User(userRequest.getUsername()));
+        return new UserResponse(savedUser.getId(), savedUser.getUsername());
+    }
+
+    @Transactional(readOnly = true)
+    public List<UserResponse> findUsers(){
+        List<User> users = userRepository.findAll();
+        List<UserResponse> userResponses = new ArrayList<>();
+        for (User user : users) {
+            UserResponse userResponse = new UserResponse(
+                    user.getId(),
+                    user.getUsername()
+            );
+            userResponses.add(userResponse);
+        }
+        return userResponses;
+    }
+
+    @Transactional
+    public UserResponse update(Long userId, UserRequest userRequest){
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new IllegalArgumentException("해당 userID " + userId + " 존재하지 않습니다.")
+        );
+        user.updateUsername(userRequest.getUsername());
+        return new UserResponse(
+                user.getId(),
+                user.getUsername());
+    }
+
+    @Transactional
+    public void delete(Long userId){
+        boolean exists = userRepository.existsById(userId);
+        if (!exists){
+            throw new IllegalArgumentException("해당 userID " + userId + " 존재하지 않습니다.");
+        }
+        userRepository.deleteById(userId);
+    }
+
+    @Transactional(readOnly = true)
+    public UserResponse findUser(Long userId){
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new IllegalArgumentException("해당 userID " + userId + " 존재하지 않습니다.")
+        );
+        return new UserResponse(user.getId(), user.getUsername());
+    }
 }
