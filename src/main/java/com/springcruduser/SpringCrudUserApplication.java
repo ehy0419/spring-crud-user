7119c051fdb7cda2ec78704d7e60dc7bf3413782
ehy0419 <2hy0419@naver.com>
2025-08-01 13:16:52 +0900

    init

---
+++ src/main/java/com/springcruduser/SpringCrudUserApplication.java
@@ -0,0 +1,13 @@
+package com.springcruduser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCrudUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springcruduser/SpringCrudUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcruduser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
